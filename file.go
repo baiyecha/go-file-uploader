f6909cbfca1739480c0f91b9ad042f665bc4372a
wq1019 <2013855675@qq.com>
2019-01-13 00:21:21 +0800

    file model add id field

--- file.go
+++ file.go
@@ -3,6 +3,7 @@ package go_file_uploader
 import "time"
 
 type FileModel struct {
+	Id        int64     `json:"id" gorm:"type:BIGINT AUTO_INCREMENT;PRIMARY_KEY;NOT NUll"`
 	Hash      string    `json:"hash" gorm:"primary_key;type:char(32)"`
 	Format    string    `json:"format" gorm:"not null"`
 	Filename  string    `json:"filename" gorm:"not null"`
